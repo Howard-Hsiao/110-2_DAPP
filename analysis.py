8da3393c1c75cf5a7a095f0932ae4631e37400f0
Yun <keepchangingtobe@gmail.com>
2022-04-06 10:45:03 +0800

    add analysis on wallet

--- analysis.py
+++ analysis.py
@@ -1,9 +1,7 @@
-from dash import Dash, dcc, html, dash_table
+from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
-import math
-import plotly.express as px
 from utils import *
 from datetime import datetime
 
@@ -14,7 +12,7 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 DISPLAY_LIMIT = 15
 initial_nft = 919
 initial_nft_data = get_NFT_record(initial_nft)
-initial_records  = initial_nft_data["records"][:DISPLAY_LIMIT]
+initial_records  = initial_nft_data["records"]
 initial_nft_df = pd.DataFrame({
     "time": [datetime.fromtimestamp(i["timestamp"]) for i in initial_records], 
     "type": [i["type"] for i in initial_records], 
@@ -23,6 +21,16 @@ initial_nft_df = pd.DataFrame({
     "address": [i["address"] for i in initial_records]
 })
 
+initial_address = "tz2VEXfSkWgA7p4X95QpYvFfQusxFgdivthm"
+ini_wallet_data = get_wallet_record(initial_address)
+ini_wallet_records  = ini_wallet_data["records"]
+op_type = "collect"
+initial_collect_df = pd.DataFrame({
+    "time": [i["timestamp"] for i in ini_wallet_records if i["type"] == op_type], 
+    "price": [i["price"] for i in ini_wallet_records if i["type"] == op_type], 
+})
+
+
 app.layout = html.Div([
     html.Div([
         html.H5('R10944033 analyze tool')
@@ -30,6 +38,7 @@ app.layout = html.Div([
     html.Div([
         html.H1('Analysis of akaswap'),
         html.Div([
+            html.H3('Analysis on NFT'), 
             html.H4('Please input tokenID'),
             dbc.Input(
                 id="NFT_address_input", 
@@ -41,7 +50,7 @@ app.layout = html.Div([
             html.Div([
                 dcc.Graph(
                     id="NFT_pie_chart",
-                    figure=pieChartByTokenId(initial_nft), 
+                    figure=pieChartByTokenId(initial_records), 
                 ),
                 html.Div([
                     html.H5("The records of a NFT", style={"text-align": "center"}), 
@@ -57,6 +66,31 @@ app.layout = html.Div([
                 "flex-direction": "row", 
                 "justify-content": "space-around"
             })
+        ]), 
+        html.Div([
+            html.H3('Analysis on Wallet'), 
+            html.H4('Please input address'),
+            dbc.Input(
+                id="wallet_address_input", 
+                placeholder='Enter Address of NFT',
+                type='text',
+                value=f'{initial_address}',
+                debounce=True
+            ), 
+            html.Div([
+                dcc.Graph(
+                    id="wallet_pie_chart",
+                    figure=pieChartByWallet(ini_wallet_records), 
+                ),
+                dcc.Graph(
+                    id="wallet_line_chart",
+                    figure=lineChartbyOp(ini_wallet_records), 
+                ),
+            ], style={
+                "display": "flex", 
+                "flex-direction": "row", 
+                "justify-content": "space-around"
+            })
         ])
     ], style={"padding": "30px"}) # , style={"border-top": "50px #1f8a8c solid", "padding": "30px"})
 ])
@@ -67,9 +101,9 @@ app.layout = html.Div([
               Input('NFT_address_input', 'value')) 
 def update_ana_NFT(NFT_tokenID):
     data = get_NFT_record(NFT_tokenID)
-    data_records  = data["records"][:DISPLAY_LIMIT]
+    data_records = data["records"]
 
-    pie_chart = pieChartByTokenId(NFT_tokenID)
+    pie_chart = pieChartByTokenId(data_records)
 
     table_df = pd.DataFrame({
         "time": [datetime.fromtimestamp(i["timestamp"]) for i in data_records], 
@@ -81,6 +115,16 @@ def update_ana_NFT(NFT_tokenID):
 
     return pie_chart, [html.H5("The records of a NFT",  style={"text-align": "center"}), dbc.Table.from_dataframe(table_df, striped=True, bordered=True, hover=True)]
 
+@app.callback(Output('wallet_pie_chart', 'figure'), 
+              Output('wallet_line_chart', 'figure'), 
+              Input('wallet_address_input', 'value')) 
+def update_ana_wallet(address):
+    data = get_wallet_record(address)
+    data_records = data["records"]
+    pie_chart = pieChartByWallet(data_records)
+    line_chart = lineChartbyOp(data_records)
+
+    return pie_chart, line_chart
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from dash import Dash, dcc, html
 import requests as rq
 import pandas as pd
 import plotly.express as px
+from sklearn.exceptions import DataConversionWarning
 
 def aka_get(api):
     return rq.get(api, verify=False)
@@ -9,13 +10,62 @@ def aka_get(api):
 def get_NFT_record(tokenId):
     return aka_get(f"https://akaswap.com/api/akaobjs/{tokenId}/records").json()
 
-def pieChartByTokenId(tokenID):
-    data = get_NFT_record(tokenID)
+
+def get_wallet_record(address):
+    return aka_get(f"https://api.akaswap.com/v2/accounts/{address}/records").json()
+
+
+def pieChartByTokenId(data):
     df = pd.DataFrame({
-        "type": [i["type"] for i in data["records"]], 
-        "address": [i["address"] for i in data["records"]]
+        "type": [i["type"] for i in data], 
+        "address": [i["address"] for i in data]
     })
     fig = px.pie(df, names="type", title='The operation made on this NFT', template="plotly_dark")
+    fig.update_layout(
+        autosize=False,
+        width=500,
+        height=500,
+        margin=dict(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad=4
+        ),
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)'
+    )
+    return fig
+
+def pieChartByWallet(data):
+    df = pd.DataFrame({
+        "type": [i["type"] for i in data] 
+    })
+    fig = px.pie(df, names="type", title='The operation made by this wallet', template="plotly_dark")
+    fig.update_layout(
+        autosize=False,
+        width=500,
+        height=500,
+        margin=dict(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad=4
+        ),
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)'
+    )
+    return fig
+
+def lineChartbyOp(data, op="collect"):
+    # data is of json format
+    df = pd.DataFrame({
+        "price": [i["price"] for i in data if i["type"] == op], 
+        "time": [i["timestamp"] for i in data if i["type"] == op], 
+    })
+
+    fig = px.line(df, x="time", y="price", title=f'Trend of {op}', template="plotly_dark")
     fig.update_layout(
         autosize=False,
         width=500,
